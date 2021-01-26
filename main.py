3a654338639e9f239a44b3bb4a086952377f0953
Umair Shaheen <109942@eesd.org>
2021-01-26 00:30:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+'''
+1/25/2021 Notes
+'''
+# Homework Review - Data Types
+
+# name = input("Enter your Name:")
+# page = input ("Enter your age:")
+# drink = input ("What is your favorite drink?")
+# grade = input("Enter your grade:")
+
+#print ("Hi, I am" + "" + name, "I am " + "" + age + "years old!" + "" + "My favorite drink is" + " " + drink + "" + "My current grade is..." + "" + grade + "!")
+
+print("hi\nbye")
+print("hi\tbye")
+
+name = input("Enter your name:")
+print ("Hi" + "" + name)
+
+'''
+Review ~ Operators
+
+1. Math Operators (Artimetic Operators)
+ Addition (+), Subtraction (-), Multipliaction (*), Divison (/ for exact answer and // for quotient), Modulo (% for remainder), and exponent (** for powers)
+ 5 / 2 = 2.5
+ 5 // 2 = 2
+ 5 % 1 = 1
+ 9 % 10 = 9
+
+2. Assignment Operators
+= assignment 
+
+ += Addition Assignment
+ -= Subtraction Assignment
+ *= Multiply Assignment
+
+ Rule: 
+ a += 4 >> a = a + 4 
+
+3. Comparison Operators
+ == equal to
+ != not equal to
+ < less than
+ <= less than or equal to
+ > greater then
+ >= greater than or equal to
+
+5 == 4 False
+6 >= 7 False
+0 != zero True
+
+
+'''
+print (6 >= 7)
+print (0 != "zero")
+
+'''
+
+if (condition):
+ BODY
+else:
+  BODY
+
+'''
+
+a = 21
+
+if (z % 3 == 0):
+ print ("Hi")
+
+else:
+ print ('Bye')
+
+if (z % 3 == 0):
